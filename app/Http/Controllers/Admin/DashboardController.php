539a2d1dd1bc65b4c838dca133486f761c7824b5
stiva98 <samustiva9810@gmail.com>
2023-09-19 18:14:48 +0200

    ADD auth in store e post e facades in dashControl

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//Helpers
+use Illuminate\Support\Facades\Auth;
+
 class DashboardController extends Controller
 {
     public function dashboard() {
